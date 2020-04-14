fc29ba3a3e9c1bf6cad4e655320bde4040821bc5
Sonia Trimble <Sonia@Sonia-Marie-Trimble.local>
2020-04-13 18:01:38 -0600

    updates

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
 
-// var workDay = {
-//     "9 AM": "",
-//     "10 AM": "",
-//     "11 AM": "",
-//     "12 PM": "",
-//     "1 PM": "",
-//     "2 PM": "",
-//     "3 PM": "",
-//     "4 PM": "",
-//     "5 PM": "",
-// };
-
 $(document).ready(function () {
 
     $(".saveBtn").on("click", function () {
@@ -24,27 +12,17 @@ $(document).ready(function () {
     })
 
 
-    // if (!localStorage.getItem('workDay')) {
-
-    //     updateCalendarTasks(workDay);
-
-    // } else {
-
-    //     updateCalendarTasks(JSON.parse(localStorage.getItem('workDay')));
-
-    // }
 
     function hourUpdater() {
 
+
         var currentHour = moment().hours();
 
-        // $(".time-block").each(function () {
 
-        //     var blockHour
 
-            $(".time-block").each(function () {
+        $(".time-block").each(function () {
 
-                var blockHour = parseInt($(this).attr("id").split("-")[1])
+            var blockHour = parseInt($(this).attr("id").split("-")[1])
 
             // })
 
@@ -102,112 +80,4 @@ $(document).ready(function () {
 
 
 })
-
-// $("#date-today h-6").text(moment().format('dddd') + " , " + moment().format("MMMM Do YYYY"));
-
-// var counter = 1;
-
-// for (const property in workDay) {
-
-//     var textEntry = "#text-entry" + counter;
-
-//     $(textEntry).text(workDay[property]);
-
-//     var timeId = "#time" + counter;
-
-//     var presentHour = moment().hour();
-
-//     var timeString = $(timeId).text();
-
-//     var timeNumber = hourNumberFromHourString(timeString);
-
-//     if (timeNumber < presentHour) {
-
-//         $(textEntry).addClass("past-hour");
-
-//     } else if (timeNumber > presentHour) {
-
-//         $(textEntry).addClass("future-hour");
-
-//     } else {
-
-//         $(textEntry).addClass("present-hour");
-
-//     }
-
-//     counter++;
-
-// }
-
-// $("button").click(function () {
-
-//     value = $(this).siblings("text area").val();
-
-//     hourString = $(this).siblings("div").text();
-
-//     saveSchedule(hourString, value);
-
-// });
-
-// function hourNumberFromHourString(hourString) {
-
-//     switch (hourString) {
-
-//         case "9 AM": return 9;
-//         case "10 AM": return 10;
-//         case "11 AM": return 11;
-//         case "12 AM": return 12;
-//         case "1 PM": return 13;
-//         case "2 PM": return 14;
-//         case "3 PM": return 15;
-//         case "4 PM": return 16;
-//         case "5 PM": return 17;
-//     }
-
-// }
-
-// function loadCorrectDataset() {
-
-//     result = localStorage.getItem('workDay')
-//     return (result ? result : workDay);
-
-// }
-
-// function initializeLocalStorage() {
-
-//     localStorage.setItem('workDay', JSON.stringify(workDay));
-
-// }
-
-// function saveToLocalStorage(dayObject) {
-
-//     localStorage.setItem('workDay', JSON.stringify(dayObj));
-
-// }
-
-// function saveSchedule(hourString, val) {
-
-//     if (!localStorage.getItem('workDay')) {
-
-//         initializeLocalStorage();
-//     }
-
-//     var workHours = JSON.parse(localStorage.getItem('workDay'));
-
-//     workHours[hourString] = val
-
-//     saveToLocalStorage(workHours);
-
-// }
-
-// function updateCalendarTasks(dayObject) {
-
-//     $(".calendar-row").each(function (index) {
-
-//         var res = $(this).append("div");
-
-//         $(this).children("text area").text(dayObject[res.text()]);
-
-//     })
-// }
 
